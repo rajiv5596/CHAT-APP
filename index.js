ce6259fa1760f425b5b699104a847f296e88645e
Rajiv <rg1362001@gmail.com>
2024-11-19 08:40:53 -0800

    new fuction added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const age = 23;
-let name = "rajiv";
+
 let dob = "13-6-2001";
+let name = "rajiv";
 
 function sayName(){
-    console.log("Hello");
+  console.log("Hello");
+}           
+
+function isAbove18(){
+   return age >= 18
 }
